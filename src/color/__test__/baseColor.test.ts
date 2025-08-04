b22c0437d27b34ac84842a6abd5afbcdfd7b96f4
Richard Teammco <teammcr192@gmail.com>
2025-08-04 11:34:58 -0400

    feat: preserve missing alpha

--- src/color/__test__/baseColor.test.ts
+++ src/color/__test__/baseColor.test.ts
@@ -3,22 +3,27 @@ import * as utils from '../utils';
 
 describe('Color', () => {
   it('should initialize color correctly from hex', () => {
-    expect(new Color('#00ff00').toRGBA()).toEqual({ r: 0, g: 255, b: 0, a: 1 });
+    expect(new Color('#00ff00').toRGBA()).toEqual({ r: 0, g: 255, b: 0 });
+    expect(new Color('#00ff00').toRGB()).toEqual({ r: 0, g: 255, b: 0 });
     expect(new Color('#00ff00').toHex()).toEqual('#00ff00');
 
-    expect(new Color('#ffffff').toRGBA()).toEqual({ r: 255, g: 255, b: 255, a: 1 });
+    expect(new Color('#ffffff').toRGBA()).toEqual({ r: 255, g: 255, b: 255 });
+    expect(new Color('#ffffff').toRGB()).toEqual({ r: 255, g: 255, b: 255 });
     expect(new Color('#ffffff').toHex()).toEqual('#ffffff');
 
-    expect(new Color('#00ffff').toRGBA()).toEqual({ r: 0, g: 255, b: 255, a: 1 });
+    expect(new Color('#00ffff').toRGBA()).toEqual({ r: 0, g: 255, b: 255 });
+    expect(new Color('#00ffff').toRGB()).toEqual({ r: 0, g: 255, b: 255 });
     expect(new Color('#00ffff').toHex()).toEqual('#00ffff');
 
     expect(new Color('#00000000').toRGBA()).toEqual({ r: 0, g: 0, b: 0, a: 0 });
+    expect(new Color('#00000000').toRGB()).toEqual({ r: 0, g: 0, b: 0 });
     expect(new Color('#00000000').toHex()).toEqual('#00000000');
   });
 
   it('should initialize color correctly from RGBA', () => {
     expect(new Color({ r: 0, g: 255, b: 0, a: 1 }).toRGBA()).toEqual({ r: 0, g: 255, b: 0, a: 1 });
-    expect(new Color({ r: 0, g: 255, b: 0, a: 1 }).toHex()).toEqual('#00ff00');
+    expect(new Color({ r: 0, g: 255, b: 0, a: 1 }).toRGB()).toEqual({ r: 0, g: 255, b: 0 });
+    expect(new Color({ r: 0, g: 255, b: 0, a: 1 }).toHex()).toEqual('#00ff00ff');
 
     expect(new Color({ r: 255, g: 255, b: 255, a: 1 }).toRGBA()).toEqual({
       r: 255,
@@ -26,7 +31,12 @@ describe('Color', () => {
       b: 255,
       a: 1,
     });
-    expect(new Color({ r: 255, g: 255, b: 255, a: 1 }).toHex()).toEqual('#ffffff');
+    expect(new Color({ r: 255, g: 255, b: 255, a: 1 }).toRGB()).toEqual({
+      r: 255,
+      g: 255,
+      b: 255,
+    });
+    expect(new Color({ r: 255, g: 255, b: 255, a: 1 }).toHex()).toEqual('#ffffffff');
 
     expect(new Color({ r: 0, g: 255, b: 255, a: 1 }).toRGBA()).toEqual({
       r: 0,
@@ -34,10 +44,22 @@ describe('Color', () => {
       b: 255,
       a: 1,
     });
-    expect(new Color({ r: 0, g: 255, b: 255, a: 1 }).toHex()).toEqual('#00ffff');
+    expect(new Color({ r: 0, g: 255, b: 255, a: 1 }).toRGB()).toEqual({
+      r: 0,
+      g: 255,
+      b: 255,
+    });
+    expect(new Color({ r: 0, g: 255, b: 255, a: 1 }).toHex()).toEqual('#00ffffff');
 
     expect(new Color({ r: 0, g: 0, b: 0, a: 0 }).toRGBA()).toEqual({ r: 0, g: 0, b: 0, a: 0 });
+    expect(new Color({ r: 0, g: 0, b: 0, a: 0 }).toRGB()).toEqual({ r: 0, g: 0, b: 0 });
     expect(new Color({ r: 0, g: 0, b: 0, a: 0 }).toHex()).toEqual('#00000000');
+  });
+
+  it('should initialize color correctly from RGB', () => {
+    expect(new Color({ r: 0, g: 255, b: 0 }).toRGBA()).toEqual({ r: 0, g: 255, b: 0 });
+    expect(new Color({ r: 0, g: 255, b: 0 }).toRGB()).toEqual({ r: 0, g: 255, b: 0 });
+    expect(new Color({ r: 0, g: 255, b: 0 }).toHex()).toEqual('#00ff00');
   });
 
   it('should initialize color correctly with no input', () => {
@@ -46,7 +68,8 @@ describe('Color', () => {
       .mockReturnValue({ r: 5, g: 10, b: 15, a: 1 });
 
     expect(new Color().toRGBA()).toEqual({ r: 5, g: 10, b: 15, a: 1 });
-    expect(new Color().toHex()).toEqual('#050a0f');
+    expect(new Color().toRGB()).toEqual({ r: 5, g: 10, b: 15 });
+    expect(new Color().toHex()).toEqual('#050a0fff');
 
     mockGetRandomColor.mockRestore();
   });
