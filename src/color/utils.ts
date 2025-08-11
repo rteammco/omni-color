ea51fb83b3ea7a4d86d504fd361f93e65071ad65
Richard Teammco <teammcr192@gmail.com>
2025-08-11 08:09:13 -0400

    Rename color enums and expand color name tests

--- src/color/utils.ts
+++ src/color/utils.ts
@@ -24,3 +24,74 @@ export function isColorDark(color: Color): boolean {
   }
   return brightness < 128;
 }
+
+export enum BaseColorName {
+  Red = 'Red',
+  Orange = 'Orange',
+  Yellow = 'Yellow',
+  Green = 'Green',
+  Blue = 'Blue',
+  Purple = 'Purple',
+  Pink = 'Pink',
+  Black = 'Black',
+  Gray = 'Gray',
+  White = 'White',
+}
+
+export enum ColorLightnessModifier {
+  Light = 'Light',
+  Normal = 'Normal',
+  Dark = 'Dark',
+}
+
+export function getBaseColorName(
+  color: Color,
+): { name: BaseColorName; lightness: ColorLightnessModifier } {
+  const { h, s, l } = color.toHSL();
+
+  if (s <= 10) {
+    if (l <= 10) {
+      return { name: BaseColorName.Black, lightness: ColorLightnessModifier.Normal };
+    }
+    if (l >= 90) {
+      return { name: BaseColorName.White, lightness: ColorLightnessModifier.Normal };
+    }
+    return { name: BaseColorName.Gray, lightness: getLightnessModifier(l) };
+  }
+
+  const name = getHueName(h);
+  const lightness = getLightnessModifier(l);
+  return { name, lightness };
+}
+
+function getLightnessModifier(l: number): ColorLightnessModifier {
+  if (l <= 30) {
+    return ColorLightnessModifier.Dark;
+  }
+  if (l >= 70) {
+    return ColorLightnessModifier.Light;
+  }
+  return ColorLightnessModifier.Normal;
+}
+
+function getHueName(h: number): BaseColorName {
+  if (h < 15 || h >= 345) {
+    return BaseColorName.Red;
+  }
+  if (h < 45) {
+    return BaseColorName.Orange;
+  }
+  if (h < 75) {
+    return BaseColorName.Yellow;
+  }
+  if (h < 165) {
+    return BaseColorName.Green;
+  }
+  if (h < 255) {
+    return BaseColorName.Blue;
+  }
+  if (h < 285) {
+    return BaseColorName.Purple;
+  }
+  return BaseColorName.Pink;
+}
