303b76c282e4f1d765d63ebc6966d8ec2e435711
Richard Teammco <teammcr192@gmail.com>
2025-08-17 14:20:49 -0400

    Choose which type of palette to generate (harmony options)

--- demo/src/playground/ColorPaletteDemo.tsx
+++ demo/src/playground/ColorPaletteDemo.tsx
@@ -1,19 +1,112 @@
-import type { Color } from '../../../dist';
+import { useState } from 'react';
+import { Color, ColorHarmony } from '../../../dist';
 import { ColorSwatch } from './ColorSwatch';
+
+const PALETTE_HARMONY_RADIO_GROUP_NAME = 'palette-color-harmony';
+
+function PaletteHarmonyOptions({
+  selectedHarmony,
+  onHarmonySelectionChanged,
+}: {
+  selectedHarmony: ColorHarmony;
+  onHarmonySelectionChanged: (harmony: ColorHarmony) => void;
+}) {
+  return (
+    <div className="w-full flex flex-row justify-center items-center gap-4">
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.COMPLEMENTARY}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.COMPLEMENTARY}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.COMPLEMENTARY)}
+        />
+        Complementary
+      </label>
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.SPLIT_COMPLEMENTARY}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.SPLIT_COMPLEMENTARY}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.SPLIT_COMPLEMENTARY)}
+        />
+        Split complementary
+      </label>
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.TRIADIC}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.TRIADIC}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.TRIADIC)}
+        />
+        Triadic
+      </label>
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.SQUARE}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.SQUARE}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.SQUARE)}
+        />
+        Square
+      </label>
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.TETRADIC}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.TETRADIC}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.TETRADIC)}
+        />
+        Tetradic
+      </label>
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.ANALOGOUS}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.ANALOGOUS}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.ANALOGOUS)}
+        />
+        Analogous
+      </label>
+      <label className="flex flex-row items-center gap-1">
+        <input
+          checked={selectedHarmony === ColorHarmony.MONOCHROMATIC}
+          name={PALETTE_HARMONY_RADIO_GROUP_NAME}
+          type="radio"
+          value={ColorHarmony.MONOCHROMATIC}
+          onChange={() => onHarmonySelectionChanged(ColorHarmony.MONOCHROMATIC)}
+        />
+        Monochromatic
+      </label>
+    </div>
+  );
+}
 
 interface Props {
   color: Color;
 }
 
 export function ColorPaletteDemo({ color }: Props) {
-  const palette = color.getColorPalette();
+  const [selectedHarmony, setSelectedHarmony] = useState(ColorHarmony.COMPLEMENTARY);
+
+  const palette = color.getColorPalette(selectedHarmony);
   /*
     secondaryColors: ColorSwatch[];
   */
+
   return (
     <div>
-      <h5 className="mb-3">Palette</h5>
-      <div className="w-full flex flex-row justify-center">
+      <h5 className="mb-2">Palette</h5>
+      <PaletteHarmonyOptions
+        selectedHarmony={selectedHarmony}
+        onHarmonySelectionChanged={setSelectedHarmony}
+      />
+      <div className="w-full mt-3 flex flex-row justify-center">
         <table className="table-auto">
           <tbody>
             <tr>
@@ -22,6 +115,16 @@ export function ColorPaletteDemo({ color }: Props) {
                 <ColorSwatch swatch={palette.primary} withLabels />
               </td>
             </tr>
+            {palette.secondaryColors.map((swatch, index) => (
+              <tr key={index}>
+                <td className="pb-2 pr-4 text-right">{`Secondary${
+                  palette.secondaryColors.length > 1 ? ` ${index + 1}` : ''
+                }`}</td>
+                <td className="pb-2">
+                  <ColorSwatch swatch={swatch} withLabels />
+                </td>
+              </tr>
+            ))}
             <tr>
               <td className="pb-2 pr-4 text-right">Neutrals</td>
               <td className="pb-2">
