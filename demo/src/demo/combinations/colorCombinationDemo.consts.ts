70b5c7a0bf5c8554247b3713c57ba1b1212bdb40
Richard Teammco <teammcr192@gmail.com>
2025-12-17 08:58:36 -0500

    add 'as const'

--- demo/src/demo/combinations/colorCombinationDemo.consts.ts
+++ demo/src/demo/combinations/colorCombinationDemo.consts.ts
@@ -3,14 +3,14 @@ import type { AverageColorsOptions, BlendColorsOptions, MixColorsOptions } from 
 export const DEFAULT_MIX_COLORS_OPTIONS: MixColorsOptions = {
   space: 'LINEAR_RGB',
   type: 'ADDITIVE',
-};
+} as const;
 
 export const DEFAULT_BLEND_COLORS_OPTIONS: BlendColorsOptions = {
   mode: 'NORMAL',
   space: 'RGB',
   ratio: 0.5,
-};
+} as const;
 
 export const DEFAULT_AVERAGE_COLORS_OPTIONS: AverageColorsOptions = {
   space: 'LINEAR_RGB',
-};
+} as const;
