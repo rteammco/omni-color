e66531fedc571feaa2863957dfa3b32c55a1af34
Richard Teammco <teammcr192@gmail.com>
2025-12-19 18:11:58 -0500

    Fix base route for demo (no need for the explicit / at the end)

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -6,5 +6,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  base: '/omni-color/',
+  base: '/omni-color',
 });
